e12b5468a584a8db28b0b1370408baf079b15ec8
pghoya2956 <pghoya2956@gmail.com>
2022-03-08 23:59:30 +0900

    Basic UI Setting and Create ViewModel and Model of MainView using INotifyPropertyChanged

---
+++ View-weather-information-at-once/View-weather-information-at-once/Model/MainModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace View_weather_information_at_once.Model
+{
+	public class MainModel : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler? PropertyChanged;
+
+		protected void OnPropertyChanged([CallerMemberName] string? name = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+		}
+
+		private int _currentView;
+		public int CurrentView 
+		{
+			get => _currentView;
+			set 
+			{ 
+				_currentView = value;
+				OnPropertyChanged();
+			}
+		}
+	}
+}
---
+++ View-weather-information-at-once/View-weather-information-at-once/ViewModel/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace View_weather_information_at_once.ViewModel
+{
+	public class MainViewModel
+	{
+		
+	}
+}
