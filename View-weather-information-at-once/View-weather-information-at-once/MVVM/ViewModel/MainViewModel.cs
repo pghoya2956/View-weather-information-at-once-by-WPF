4f26cd4ea983a5c5ec684319fde8d269c88ae3d6
pghoya2956 <pghoya2956@gmail.com>
2022-03-11 23:24:48 +0900

    Folder Path Changed

---
+++ View-weather-information-at-once/View-weather-information-at-once/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using View_weather_information_at_once.Core;
+using View_weather_information_at_once.MVVM.Model;
+
+namespace View_weather_information_at_once.MVVM.ViewModel
+{
+	public class MainViewModel
+	{
+		public MainModel MainModel { get; set; } = new MainModel();
+
+
+		public MainViewModel()
+		{
+			MainModel.OverViewCommand = new RelayCommand(o =>
+			{
+				MainModel.OverViewVM = new OverViewViewModel();
+				MainModel.CurrentView = MainModel.OverViewVM;
+			});
+
+
+		}
+
+	}
+}
---
+++ View-weather-information-at-once/View-weather-information-at-once/MVVM/ViewModel/OverViewViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace View_weather_information_at_once.MVVM.ViewModel
+{
+	public class OverViewViewModel
+	{
+	}
+}
